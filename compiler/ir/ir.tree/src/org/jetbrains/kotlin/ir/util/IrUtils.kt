eggfly/kotlin

[IR] Migrate ir.tree to new IR parameter API A33

compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/IrUtils.kt
@@ -376,8 +376,8 @@ fun IrAnnotationContainer.hasAnnotation(symbol: IrClassSymbol) =
 fun IrConstructorCall.getAnnotationStringValue() = (arguments[0] as? IrConst)?.value as String?
 
 fun IrConstructorCall.getAnnotationStringValue(name: String): String {
-    val parameter = symbol.owner.valueParameters.single { it.name.asString() == name }
-    return (getValueArgument(parameter.index) as IrConst).value as String
+    val parameter = symbol.owner.parameters.single { it.name.asString() == name }
+    return (arguments[parameter.indexNew] as IrConst).value as String
 }
 
 inline fun <reified T> IrConstructorCall.getAnnotationValueOrNull(name: String): T? =

slowcoders/kotlin-nstw

compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/IrUtils.kt
@@ -376,8 +376,8 @@ fun IrAnnotationContainer.hasAnnotation(symbol: IrClassSymbol) =
 fun IrConstructorCall.getAnnotationStringValue() = (arguments[0] as? IrConst)?.value as String?
 
 fun IrConstructorCall.getAnnotationStringValue(name: String): String {
-    val parameter = symbol.owner.valueParameters.single { it.name.asString() == name }
-    return (getValueArgument(parameter.index) as IrConst).value as String
+    val parameter = symbol.owner.parameters.single { it.name.asString() == name }
+    return (arguments[parameter.indexInParameters] as IrConst).value as String
 }
 
 inline fun <reified T> IrConstructorCall.getAnnotationValueOrNull(name: String): T? =