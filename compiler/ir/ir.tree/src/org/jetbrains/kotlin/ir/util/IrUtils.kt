eggfly/kotlin

[IR] Migrate ir.tree to new IR parameter API A41

compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/IrUtils.kt
@@ -762,14 +762,7 @@ fun IrGetValue.remapSymbolParent(classRemapper: (IrClass) -> IrClass, functionRe
 
         is IrFunction -> {
             val remappedFunction = functionRemapper(parent)
-            when (parameter) {
-                parent.dispatchReceiverParameter -> remappedFunction.dispatchReceiverParameter!!
-                parent.extensionReceiverParameter -> remappedFunction.extensionReceiverParameter!!
-                else -> {
-                    assert(parent.valueParameters[parameter.index] == parameter)
-                    remappedFunction.valueParameters[parameter.index]
-                }
-            }
+            remappedFunction.parameters[parameter.indexNew]
         }
 
         else -> error(parent)

slowcoders/kotlin-nstw

compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/IrUtils.kt
@@ -785,14 +785,7 @@ fun IrGetValue.remapSymbolParent(classRemapper: (IrClass) -> IrClass, functionRe
 
         is IrFunction -> {
             val remappedFunction = functionRemapper(parent)
-            when (parameter) {
-                parent.dispatchReceiverParameter -> remappedFunction.dispatchReceiverParameter!!
-                parent.extensionReceiverParameter -> remappedFunction.extensionReceiverParameter!!
-                else -> {
-                    assert(parent.valueParameters[parameter.index] == parameter)
-                    remappedFunction.valueParameters[parameter.index]
-                }
-            }
+            remappedFunction.parameters[parameter.indexInParameters]
         }
 
         else -> error(parent)