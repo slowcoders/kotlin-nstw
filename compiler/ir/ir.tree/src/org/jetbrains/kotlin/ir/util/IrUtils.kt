eggfly/kotlin

[IR] Migrate ir.tree to new IR parameter API A34

compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/IrUtils.kt
@@ -385,8 +385,8 @@ inline fun <reified T> IrConstructorCall.getAnnotationValueOrNull(name: String):
 
 @PublishedApi
 internal fun IrConstructorCall.getAnnotationValueOrNullImpl(name: String): Any? {
-    val parameter = symbol.owner.valueParameters.atMostOne { it.name.asString() == name }
-    val argument = parameter?.let { getValueArgument(it.index) }
+    val parameter = symbol.owner.parameters.atMostOne { it.name.asString() == name }
+    val argument = parameter?.let { arguments[it.indexNew] }
     return (argument as IrConst?)?.value
 }
 

slowcoders/kotlin-nstw

compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/IrUtils.kt
@@ -385,8 +385,8 @@ inline fun <reified T> IrConstructorCall.getAnnotationValueOrNull(name: String):
 
 @PublishedApi
 internal fun IrConstructorCall.getAnnotationValueOrNullImpl(name: String): Any? {
-    val parameter = symbol.owner.valueParameters.atMostOne { it.name.asString() == name }
-    val argument = parameter?.let { getValueArgument(it.index) }
+    val parameter = symbol.owner.parameters.atMostOne { it.name.asString() == name }
+    val argument = parameter?.let { arguments[it.indexInParameters] }
     return (argument as IrConst?)?.value
 }
 