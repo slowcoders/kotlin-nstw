eggfly/kotlin

[IR] Migrate ir.tree to new IR parameter API A42

compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/IrUtils.kt
@@ -801,7 +801,7 @@ fun IrClass.addSimpleDelegatingConstructor(
         this.visibility = superConstructor.visibility
         this.isPrimary = isPrimary
     }.also { constructor ->
-        constructor.valueParameters = superConstructor.valueParameters.memoryOptimizedMapIndexed { index, parameter ->
+        constructor.parameters = superConstructor.parameters.memoryOptimizedMap { parameter ->
             parameter.copyTo(constructor)
         }
 
@@ -812,8 +812,8 @@ fun IrClass.addSimpleDelegatingConstructor(
                     startOffset, endOffset, irBuiltIns.unitType,
                     superConstructor.symbol, 0
                 ).apply {
-                    constructor.valueParameters.forEachIndexed { idx, parameter ->
-                        putValueArgument(idx, IrGetValueImpl(startOffset, endOffset, parameter.type, parameter.symbol))
+                    constructor.parameters.forEach { parameter ->
+                        arguments[parameter.indexNew] = IrGetValueImpl(startOffset, endOffset, parameter.type, parameter.symbol)
                     }
                 },
                 IrInstanceInitializerCallImpl(startOffset, endOffset, this.symbol, irBuiltIns.unitType)

slowcoders/kotlin-nstw

compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/IrUtils.kt
@@ -824,7 +824,7 @@ fun IrClass.addSimpleDelegatingConstructor(
         this.visibility = superConstructor.visibility
         this.isPrimary = isPrimary
     }.also { constructor ->
-        constructor.valueParameters = superConstructor.valueParameters.memoryOptimizedMapIndexed { index, parameter ->
+        constructor.parameters = superConstructor.parameters.memoryOptimizedMap { parameter ->
             parameter.copyTo(constructor)
         }
 
@@ -835,8 +835,8 @@ fun IrClass.addSimpleDelegatingConstructor(
                     startOffset, endOffset, irBuiltIns.unitType,
                     superConstructor.symbol, 0
                 ).apply {
-                    constructor.valueParameters.forEachIndexed { idx, parameter ->
-                        putValueArgument(idx, IrGetValueImpl(startOffset, endOffset, parameter.type, parameter.symbol))
+                    constructor.parameters.forEach { parameter ->
+                        arguments[parameter.indexInParameters] = IrGetValueImpl(startOffset, endOffset, parameter.type, parameter.symbol)
                     }
                 },
                 IrInstanceInitializerCallImpl(startOffset, endOffset, this.symbol, irBuiltIns.unitType)