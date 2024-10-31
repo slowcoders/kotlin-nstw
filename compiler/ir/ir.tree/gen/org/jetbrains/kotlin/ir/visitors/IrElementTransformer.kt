tuke-code/kotlin

Introduce new nodes

compiler/ir/ir.tree/gen/org/jetbrains/kotlin/ir/visitors/IrElementTransformer.kt
@@ -173,6 +173,12 @@ interface IrElementTransformer<in D> : IrElementVisitor<IrElement, D> {
     override fun visitLocalDelegatedPropertyReference(expression: IrLocalDelegatedPropertyReference, data: D): IrElement =
         visitCallableReference(expression, data)
 
+    override fun visitAdaptedFunctionReference(expression: IrAdaptedFunctionReference, data: D): IrExpression =
+        visitExpression(expression, data)
+
+    override fun visitAdaptedPropertyReference(expression: IrAdaptedPropertyReference, data: D): IrExpression =
+        visitExpression(expression, data)
+
     override fun visitClassReference(expression: IrClassReference, data: D): IrExpression =
         visitDeclarationReference(expression, data)
 

compiler/ir/ir.tree/gen/org/jetbrains/kotlin/ir/visitors/IrElementTransformerVoid.kt
@@ -322,6 +322,18 @@ abstract class IrElementTransformerVoid : IrElementTransformer<Nothing?> {
     final override fun visitLocalDelegatedPropertyReference(expression: IrLocalDelegatedPropertyReference, data: Nothing?): IrElement =
         visitLocalDelegatedPropertyReference(expression)
 
+    open fun visitAdaptedFunctionReference(expression: IrAdaptedFunctionReference): IrExpression =
+        visitExpression(expression)
+
+    final override fun visitAdaptedFunctionReference(expression: IrAdaptedFunctionReference, data: Nothing?): IrExpression =
+        visitAdaptedFunctionReference(expression)
+
+    open fun visitAdaptedPropertyReference(expression: IrAdaptedPropertyReference): IrExpression =
+        visitExpression(expression)
+
+    final override fun visitAdaptedPropertyReference(expression: IrAdaptedPropertyReference, data: Nothing?): IrExpression =
+        visitAdaptedPropertyReference(expression)
+
     open fun visitClassReference(expression: IrClassReference): IrExpression =
         visitDeclarationReference(expression)
 

compiler/ir/ir.tree/gen/org/jetbrains/kotlin/ir/visitors/IrElementVisitor.kt
@@ -157,6 +157,12 @@ interface IrElementVisitor<out R, in D> {
     fun visitLocalDelegatedPropertyReference(expression: IrLocalDelegatedPropertyReference, data: D): R =
         visitCallableReference(expression, data)
 
+    fun visitAdaptedFunctionReference(expression: IrAdaptedFunctionReference, data: D): R =
+        visitExpression(expression, data)
+
+    fun visitAdaptedPropertyReference(expression: IrAdaptedPropertyReference, data: D): R =
+        visitExpression(expression, data)
+
     fun visitClassReference(expression: IrClassReference, data: D): R =
         visitDeclarationReference(expression, data)
 

compiler/ir/ir.tree/gen/org/jetbrains/kotlin/ir/visitors/IrElementVisitorVoid.kt
@@ -392,6 +392,22 @@ interface IrElementVisitorVoid : IrElementVisitor<Unit, Nothing?> {
         visitCallableReference(expression)
     }
 
+    override fun visitAdaptedFunctionReference(expression: IrAdaptedFunctionReference, data: Nothing?) {
+        visitAdaptedFunctionReference(expression)
+    }
+
+    fun visitAdaptedFunctionReference(expression: IrAdaptedFunctionReference) {
+        visitExpression(expression)
+    }
+
+    override fun visitAdaptedPropertyReference(expression: IrAdaptedPropertyReference, data: Nothing?) {
+        visitAdaptedPropertyReference(expression)
+    }
+
+    fun visitAdaptedPropertyReference(expression: IrAdaptedPropertyReference) {
+        visitExpression(expression)
+    }
+
     override fun visitClassReference(expression: IrClassReference, data: Nothing?) {
         visitClassReference(expression)
     }

slowcoders/kotlin-nstw

compiler/ir/ir.tree/gen/org/jetbrains/kotlin/ir/visitors/IrElementTransformer.kt
@@ -177,6 +177,12 @@ interface IrElementTransformer<in D> : IrElementVisitor<IrElement, D> {
     override fun visitLocalDelegatedPropertyReference(expression: IrLocalDelegatedPropertyReference, data: D): IrElement =
         visitCallableReference(expression, data)
 
+    override fun visitBoundFunctionReference(expression: IrBoundFunctionReference, data: D): IrExpression =
+        visitExpression(expression, data)
+
+    override fun visitBoundPropertyReference(expression: IrBoundPropertyReference, data: D): IrExpression =
+        visitExpression(expression, data)
+
     override fun visitClassReference(expression: IrClassReference, data: D): IrExpression =
         visitDeclarationReference(expression, data)
 

compiler/ir/ir.tree/gen/org/jetbrains/kotlin/ir/visitors/IrElementTransformerVoid.kt
@@ -322,6 +322,18 @@ abstract class IrElementTransformerVoid : IrTransformer<Nothing?>(), IrElementTr
     final override fun visitLocalDelegatedPropertyReference(expression: IrLocalDelegatedPropertyReference, data: Nothing?): IrElement =
         visitLocalDelegatedPropertyReference(expression)
 
+    open fun visitBoundFunctionReference(expression: IrBoundFunctionReference): IrExpression =
+        visitExpression(expression)
+
+    final override fun visitBoundFunctionReference(expression: IrBoundFunctionReference, data: Nothing?): IrExpression =
+        visitBoundFunctionReference(expression)
+
+    open fun visitBoundPropertyReference(expression: IrBoundPropertyReference): IrExpression =
+        visitExpression(expression)
+
+    final override fun visitBoundPropertyReference(expression: IrBoundPropertyReference, data: Nothing?): IrExpression =
+        visitBoundPropertyReference(expression)
+
     open fun visitClassReference(expression: IrClassReference): IrExpression =
         visitDeclarationReference(expression)
 

compiler/ir/ir.tree/gen/org/jetbrains/kotlin/ir/visitors/IrElementVisitor.kt
@@ -161,6 +161,12 @@ interface IrElementVisitor<out R, in D> {
     fun visitLocalDelegatedPropertyReference(expression: IrLocalDelegatedPropertyReference, data: D): R =
         visitCallableReference(expression, data)
 
+    fun visitBoundFunctionReference(expression: IrBoundFunctionReference, data: D): R =
+        visitExpression(expression, data)
+
+    fun visitBoundPropertyReference(expression: IrBoundPropertyReference, data: D): R =
+        visitExpression(expression, data)
+
     fun visitClassReference(expression: IrClassReference, data: D): R =
         visitDeclarationReference(expression, data)
 

compiler/ir/ir.tree/gen/org/jetbrains/kotlin/ir/visitors/IrElementVisitorVoid.kt
@@ -396,6 +396,22 @@ interface IrElementVisitorVoid : IrElementVisitor<Unit, Nothing?> {
         visitCallableReference(expression)
     }
 
+    override fun visitBoundFunctionReference(expression: IrBoundFunctionReference, data: Nothing?) {
+        visitBoundFunctionReference(expression)
+    }
+
+    fun visitBoundFunctionReference(expression: IrBoundFunctionReference) {
+        visitExpression(expression)
+    }
+
+    override fun visitBoundPropertyReference(expression: IrBoundPropertyReference, data: Nothing?) {
+        visitBoundPropertyReference(expression)
+    }
+
+    fun visitBoundPropertyReference(expression: IrBoundPropertyReference) {
+        visitExpression(expression)
+    }
+
     override fun visitClassReference(expression: IrClassReference, data: Nothing?) {
         visitClassReference(expression)
     }