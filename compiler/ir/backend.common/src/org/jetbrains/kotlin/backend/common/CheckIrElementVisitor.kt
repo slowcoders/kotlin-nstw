tuke-code/kotlin

[IR] Introduce new IR parameter API
`parameters` is a new way to store and access value parameters in
IrFunction. It is just a flat list of all kinds of parameters, where
the exact kind (e.g. dispatch receiver, regular parameter) is designated
by `IrValueParameter.kind`.

It is meant to replace the old API, namely:
- valueParameters
- contextReceiverParametersCount
- dispatchReceiverParameter, extensionReceiverParameter

The old API is still there, but implemented atop of `parameters`.

Migration to the new API will follow.

#KT-71582 Fixed

compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/CheckIrElementVisitor.kt
@@ -21,7 +21,7 @@ typealias ReportError = (element: IrElement, message: String) -> Unit
 internal class CheckIrElementVisitor(
     val irBuiltIns: IrBuiltIns,
     val reportError: ReportError,
-    val config: IrValidatorConfig
+    val config: IrValidatorConfig,
 ) : IrElementVisitorVoid {
     private val visitedElements = hashSetOf<IrElement>()
 
@@ -204,7 +204,8 @@ internal class CheckIrElementVisitor(
             IrTypeOperator.IMPLICIT_INTEGER_COERCION,
             IrTypeOperator.SAM_CONVERSION,
             IrTypeOperator.IMPLICIT_DYNAMIC_CAST,
-            IrTypeOperator.REINTERPRET_CAST ->
+            IrTypeOperator.REINTERPRET_CAST,
+                ->
                 typeOperand
 
             IrTypeOperator.SAFE_CAST ->
@@ -252,15 +253,42 @@ internal class CheckIrElementVisitor(
         super.visitFunction(declaration)
         declaration.checkFunction(declaration)
 
-        for ((i, p) in declaration.valueParameters.withIndex()) {
-            if (p.index != i) {
-                reportError(declaration, "Inconsistent index of value parameter ${p.index} != $i")
+        for ((i, param) in declaration.valueParameters.withIndex()) {
+            if (param.index != i) {
+                reportError(declaration, "Inconsistent index (old API) of value parameter ${param.index} != $i")
             }
         }
 
-        for ((i, p) in declaration.typeParameters.withIndex()) {
-            if (p.index != i) {
-                reportError(declaration, "Inconsistent index of type parameter ${p.index} != $i")
+        var lastKind: IrParameterKind? = null
+        for ((i, param) in declaration.parameters.withIndex()) {
+            if (param.indexNew != i) {
+                reportError(declaration, "Inconsistent index (new API) of value parameter ${param.indexNew} != $i")
+            }
+
+            val kind = param.kind
+            if (lastKind != null) {
+                if (kind < lastKind) {
+                    reportError(
+                        declaration,
+                        "Invalid order of function parameters: $kind is placed after ${lastKind}.\n" +
+                                "Parameters must follow a strict order: " +
+                                "[dispatch receiver, context parameters, extension receiver, regular parameters]."
+                    )
+                }
+
+                if (!kind.canHaveMultiple) {
+                    if (kind == lastKind) {
+                        reportError(declaration, "Function may have only one $kind parameter")
+                    }
+                }
+            }
+
+            lastKind = kind
+        }
+
+        for ((i, param) in declaration.typeParameters.withIndex()) {
+            if (param.index != i) {
+                reportError(declaration, "Inconsistent index of type parameter ${param.index} != $i")
             }
         }
     }

slowcoders/kotlin-nstw

compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/CheckIrElementVisitor.kt
@@ -253,15 +253,42 @@ internal class CheckIrElementVisitor(
         super.visitFunction(declaration)
         declaration.checkFunction(declaration)
 
-        for ((i, p) in declaration.valueParameters.withIndex()) {
-            if (p.index != i) {
-                reportError(declaration, "Inconsistent index of value parameter ${p.index} != $i")
+        for ((i, param) in declaration.valueParameters.withIndex()) {
+            if (param.index != i) {
+                reportError(declaration, "Inconsistent index (old API) of value parameter ${param.index} != $i")
             }
         }
 
-        for ((i, p) in declaration.typeParameters.withIndex()) {
-            if (p.index != i) {
-                reportError(declaration, "Inconsistent index of type parameter ${p.index} != $i")
+        var lastKind: IrParameterKind? = null
+        for ((i, param) in declaration.parameters.withIndex()) {
+            if (param.indexInParameters != i) {
+                reportError(declaration, "Inconsistent index (new API) of value parameter ${param.indexInParameters} != $i")
+            }
+
+            val kind = param.kind
+            if (lastKind != null) {
+                if (kind < lastKind) {
+                    reportError(
+                        declaration,
+                        "Invalid order of function parameters: $kind is placed after ${lastKind}.\n" +
+                                "Parameters must follow a strict order: " +
+                                "[dispatch receiver, context parameters, extension receiver, regular parameters]."
+                    )
+                }
+
+                if (!kind.canHaveMultiple) {
+                    if (kind == lastKind) {
+                        reportError(declaration, "Function may have only one $kind parameter")
+                    }
+                }
+            }
+
+            lastKind = kind
+        }
+
+        for ((i, param) in declaration.typeParameters.withIndex()) {
+            if (param.index != i) {
+                reportError(declaration, "Inconsistent index of type parameter ${param.index} != $i")
             }
         }
     }