eggfly/kotlin

[IR] Migrate ir.tree to new IR parameter API A52

compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/expressions/IrExpressions.kt
@@ -96,9 +96,10 @@ val CallableDescriptor.typeParametersCount: Int
             else -> typeParameters.size
         }
 
-fun IrMemberAccessExpression<*>.putArgument(callee: IrFunction, parameter: IrValueParameter, argument: IrExpression) =
-    when (parameter) {
-        callee.dispatchReceiverParameter -> dispatchReceiver = argument
-        callee.extensionReceiverParameter -> extensionReceiver = argument
-        else -> putValueArgument(parameter.index, argument)
-    }
+fun IrMemberAccessExpression<*>.putArgument(
+    @Suppress("unused") callee: IrFunction, // To be removed
+    parameter: IrValueParameter,
+    argument: IrExpression
+) {
+    arguments[parameter.indexNew] = argument
+}

slowcoders/kotlin-nstw

compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/expressions/IrExpressions.kt
@@ -96,9 +96,10 @@ val CallableDescriptor.typeParametersCount: Int
             else -> typeParameters.size
         }
 
-fun IrMemberAccessExpression<*>.putArgument(callee: IrFunction, parameter: IrValueParameter, argument: IrExpression) =
-    when (parameter) {
-        callee.dispatchReceiverParameter -> dispatchReceiver = argument
-        callee.extensionReceiverParameter -> extensionReceiver = argument
-        else -> putValueArgument(parameter.index, argument)
-    }
+fun IrMemberAccessExpression<*>.putArgument(
+    @Suppress("unused") callee: IrFunction, // To be removed
+    parameter: IrValueParameter,
+    argument: IrExpression
+) {
+    arguments[parameter.indexInParameters] = argument
+}